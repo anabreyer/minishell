01168d42777156f9e5c087d80cb1a4d2b4ee8154
Ana Breyer <anabreyer@MacBook-Pro-de-Ana.local>
2023-01-18 18:01:44 +0100

    beging of header file

---
+++ minishell.h
@@ -0,0 +1,17 @@
+#ifndef MINISHELL_H
+#define MINISHEL_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <sys/wait.h>
+# include <limits.h>
+# include <errno.h>
+# include <signal.h>
+
+
+
+#endif
