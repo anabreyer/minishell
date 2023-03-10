53bb82e40dd4ae0a212e1e01af07ef85e9cdfeb8
Ana Breyer <anabreyer@MacBook-Pro-de-Ana.local>
2023-03-10 19:07:59 +0100

    adding the libraries

---
+++ include/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaduan-b <aaduan-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/10 19:06:45 by aaduan-b          #+#    #+#             */
+/*   Updated: 2023/03/10 19:07:37 by aaduan-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+# include "libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdbool.h>
+
+#endif
